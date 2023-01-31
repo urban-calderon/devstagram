0f3b4cbbbfd9ad39d83a48173ad907e19f9eca30
urbancalderon3 <urbancalderon3@gmail.com>
2023-01-31 16:38:42 -0600

    ADDED: Controller for register user

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    //
+    public function index () 
+    {
+        return view('auth.register');
+    }
+
+    public function store (Request $request) 
+    {
+        //dd($request);
+        //dd($request->get('username'));
+
+        // Validación
+        $this->validate($request, [
+            'name' => 'required|min:5',
+        ]);
+    }
+}
