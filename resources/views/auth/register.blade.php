3e9018c0216fd03615c5ba700a68ffde01a0acaf
urbancalderon3 <urbancalderon3@gmail.com>
2023-01-31 16:40:16 -0600

    ADDED: create user registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,3 +3,86 @@
 @section('titulo')
     Regístrate en DevStagram
 @endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-6/12 p-5">
+            <img src="{{ asset('img/registrar.jpg') }}" alt="Imagen registro de usuarios">
+        </div>
+
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form action="/crear-cuenta" method="POST">
+                @csrf
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input 
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Tu nombre"
+                        class="border p-3 w-full rounded-lg"/>
+                    
+                    @error('name')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">{{ $message }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input 
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu nombre de usuario"
+                        class="border p-3 w-full rounded-lg"/>
+                </div>
+
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input 
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Tu email de registro"
+                        class="border p-3 w-full rounded-lg"/>
+                </div>
+
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Password
+                    </label>
+                    <input 
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Password de registro"
+                        class="border p-3 w-full rounded-lg"/>
+                </div>
+
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Repetir password
+                    </label>
+                    <input 
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        type="password"
+                        placeholder="Repite tu password"
+                        class="border p-3 w-full rounded-lg"/>
+                </div>
+
+                <input 
+                    type="submit"
+                    value="Crear cuenta"
+                    class="bg-sky-600 hover:bg-sky-700 trnasition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                    />
+            </form>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,8 +29,8 @@
             @yield('contenido')
         </main>
 
-        <footer class="text-center p-5 text-gray-500 font-bold uppercase">
-            DevStagram - Todos los derechos reervados {{ now()->year }}
+        <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
+            DevStagram - Todos los derechos reservados {{ now()->year }}
         </footer>
 
     </body>
