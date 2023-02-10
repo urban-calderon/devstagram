dc2b7ff1b868b5c314f174438a2fc7c40edcf70e
urbancalderon3 <urbancalderon3@gmail.com>
2023-02-10 13:55:40 -0600

    ADDED: new route for redirect page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,4 +26,4 @@ Route::get('/', function () {
 Route::get('/crear-cuenta', [RegisterController::class, 'index']);
 Route::post('/crear-cuenta', [RegisterController::class, 'store']);
 
-
+Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
