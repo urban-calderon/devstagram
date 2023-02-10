beb34eb8775984fa22365f9277d7bd2107a88b76
urbancalderon3 <urbancalderon3@gmail.com>
2023-02-10 13:53:52 -0600

    ADDED: new model data

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -17,12 +20,25 @@ class RegisterController extends Controller
         //dd($request);
         //dd($request->get('username'));
 
+        // Modificar el Request
+        $request->request->add(['username' => Str::slug($request->username)]);
+
         // Validación
         $this->validate($request, [
             'name' => 'required|max:30',
             'username' => 'required|unique:users|min:3|max:20',
             'email' => 'required|unique:users|email|max:60',
-            'password' => 'required'
+            'password' => 'required|confirmed:min:6'
         ]);
+
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // Redireccionar
+        return redirect()->route('posts.index');
     }
 }
