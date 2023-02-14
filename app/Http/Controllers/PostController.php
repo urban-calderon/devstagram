30d71582c3fee2464f4de7b5df2b32682efd442a
urbancalderon3 <urbancalderon3@gmail.com>
2023-02-14 15:56:14 -0600

    ADDED: create functions to authenticate a user and protect the wall path

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() 
     {
-        dd('Desde muro');
+        return view('dashboard');
     }
 }
